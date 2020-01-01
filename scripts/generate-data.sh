399933757788cfcba250739e1ef5807bc8d6172a
Manoj Shanmugasundaram <manoj.sundaram21@gmail.com>
2020-01-01 05:26:01 +0000

    completed version 1 - with wrapper shell script

---
+++ scripts/generate-data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+rm -rf /data/
+mkdir -p /data/reference_data /data/transactions
+cp ../fake-cctxn-gen/demographic_data/* /data/reference_data
+python3 ../fake-cctxn-gen/pos_merchant_generator.py >> /data/reference_data/merchants.csv
+python3 ../fake-cctxn-gen/datagen_customer.py 10000 4444 ../fake-cctxn-gen/profiles/main_config.json >> /data/reference_data/customers.csv
+
+# Enter your allowed profiles
+for i in adults_2550_female_rural.json adults_2550_female_urban.json adults_2550_male_rural.json adults_2550_male_urban.json adults_50up_female_rural.json adults_50up_female_urban.json adults_50up_male_rural.json adults_50up_male_urban.json young_adults_female_rural.json young_adults_female_urban.json young_adults_male_rural.json young_adults_male_urban.json
+do
+	fname=`echo $i | awk -F "." '{print $1}'`
+	python3 ../fake-cctxn-gen/datagen_transaction.py /data/reference_data/customers.csv ../fake-cctxn-gen/profiles/$i 12-30-2018 12-30-2019 >> /data/transactions/$fname.csv &
+done
+set +x
