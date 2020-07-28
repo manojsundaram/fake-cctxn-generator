b209a688143d95952b6ca3692ae388392bb5051e
Wendell Bu <wbu@hortonworks.com>
2020-07-28 11:24:03 +1000

    Fix Generator
    * Add parameters in 01-generate-customerdata.sh
    * Write a few transactions into a new file for NiFi easier consume

--- fake-cctxn-gen/datagen_transaction.py
+++ fake-cctxn-gen/datagen_transaction.py
@@ -6,6 +6,7 @@ import json
 import numpy as np
 import sys
 import datetime
+import time
 from datetime import timedelta
 from datetime import date
 import math
@@ -38,8 +39,10 @@ def get_user_input():
             print('Invalid date (MM-DD-YYYY)')
         output = 'ENTER:\n(1) Customers csv file\n'
         output += '(2) profile json file\n'
-        output += '(3) Start date (MM-DD-YYYY)\n'
-        output += '(4) End date (MM-DD-YYYY)\n'
+        output += '(3) Base Home Directory\n'
+        output += '(4) Start date (MM-DD-YYYY)\n'
+        output += '(5) End date (MM-DD-YYYY)\n'
+        output += '(6) Base File Name\n'
         print(output)
         sys.exit(0)
 
@@ -76,15 +79,23 @@ def get_user_input():
     except:
         error_msg(2)
     try:
-        startd = convert_date(sys.argv[3])
+        base_home = str(sys.argv[3])
     except:
         error_msg(3)
     try:
-        endd = convert_date(sys.argv[4])
+        startd = convert_date(sys.argv[4])
     except:
         error_msg(4)
+    try:
+        endd = convert_date(sys.argv[5])
+    except:
+        error_msg(5)
+    try:
+        base_file_name = str(sys.argv[6])
+    except:
+        error_msg(6)
 
-    return cstmr, pro, pro_fraud, pro_name, pro_name_fraud, startd, endd, m
+    return cstmr, pro, pro_fraud, pro_name, pro_name_fraud, base_home, startd, endd, m, base_file_name
 
 def create_header(line):
     headers = line.split('|')
@@ -143,15 +154,18 @@ class Customer:
                 merch_lat = faker.coordinate(center=float(cust_lat),radius=rad)
                 merch_long = faker.coordinate(center=float(cust_long),radius=rad)
 
+            csv_lines = ''
             if is_fraud == 0 and groups[1] not in fraud_dates:
             # if cust.attrs['profile'] == "male_30_40_smaller_cities.json":
-                print(tid + '|' + self.customer.replace('\n','') + '|' + t + '|' + str(chosen_merchant) + '|' + str(merch_lat) + '|' + str(merch_long))
+                csv_lines += (tid + '|' + self.customer.replace('\n','') + '|' + t + '|' + str(chosen_merchant) + '|' + str(merch_lat) + '|' + str(merch_long) + '\n')
 
             if is_fraud ==1:
-                print(tid + '|' + self.customer.replace('\n','') + '|' + t + '|' + str(chosen_merchant) + '|' + str(merch_lat) + '|' + str(merch_long))
+                csv_lines += (tid + '|' + self.customer.replace('\n','') + '|' + t + '|' + str(chosen_merchant) + '|' + str(merch_lat) + '|' + str(merch_long) + '\n')
 
             #else:
                 # pass
+            return csv_lines
+
 
     def clean_line(self, line):
         # separate into a list of attrs
@@ -167,7 +181,7 @@ if __name__ == '__main__':
     # to prepare the user inputs
     # curr_profile is female_30_40_smaller_cities.json , for fraud as well as non fraud
     # profile_name is ./profiles/fraud_female_30_40_bigger_cities.json for fraud.
-    cstmr, pro, pro_fraud, curr_profile, curr_fraud_profile, start, end, profile_name = get_user_input()
+    cstmr, pro, pro_fraud, curr_profile, curr_fraud_profile, base_home, start, end, profile_name, base_file_name = get_user_input()
     #if curr_profile == "male_30_40_smaller_cities.json":
     #   inputCat = "travel"
     #elif curr_profile == "female_30_40_smaller_cities.json":
@@ -187,14 +201,22 @@ if __name__ == '__main__':
     # generate Faker object to calc merchant transaction locations
     fake = Faker()
 
+    # merchants_reference_path
+    merchants_reference_path = base_home + "/data/reference_data/merchants.csv"
+
     # for each customer, if the customer fits this profile
     # generate appropriate number of transactions
+    i = 0
+    csv_lines = (''.join([h + '|' for h in headers])[:-1] + '\n')
     for line in cstmr[1:]:
+
+            i += 1
+
             profile = profile_weights.Profile(pro, start, end)
             cust = Customer(line, profile)
             #print(cust.attrs['profile'])
             if cust.attrs['profile'] == curr_profile:
-                merch = pd.read_csv('/data/reference_data/merchants.csv', sep='|')
+                merch = pd.read_csv(merchants_reference_path, sep='|')
                 is_fraud= 0
 
                 fraud_flag = randint(0,100) # set fraud flag here, as we either gen real or fraud, not both for
@@ -215,15 +237,13 @@ if __name__ == '__main__':
                     # we assume that the fraud window can be between 1 to 7 days #7->1
                     profile = profile_weights.Profile(pro_fraud, newstart, newend)
                     cust = Customer(line, profile)
-                    merch = pd.read_csv('/data/reference_data/merchants.csv' , sep='|')
+                    merch = pd.read_csv(merchants_reference_path, sep='|')
                     is_fraud = 1
                     temp_tx_data = profile.sample_from(is_fraud)
                     fraud_dates = temp_tx_data[3]
 
-
-                    cust.print_trans(temp_tx_data,is_fraud, fraud_dates)
-
-
+                    if cust is not None:
+                        csv_lines += str(cust.print_trans(temp_tx_data,is_fraud, fraud_dates))
 
                     #parse_index = m.index('profiles/') + 9
                     #m = m[:parse_index] +'fraud_' + m[parse_index:]
@@ -232,12 +252,17 @@ if __name__ == '__main__':
                 # we pass through our previously selected fraud dates (if any) to filter them
                 # out of regular transactions
                 profile = profile_weights.Profile(pro, start, end)
-                merch = pd.read_csv('/data/reference_data/merchants.csv', sep='|')
+                merch = pd.read_csv(merchants_reference_path, sep='|')
                 is_fraud = 0
                 temp_tx_data = profile.sample_from(is_fraud)
 
+                if cust is not None:
+                    csv_lines += str(cust.print_trans(temp_tx_data, is_fraud, fraud_dates))
 
-                cust.print_trans(temp_tx_data, is_fraud, fraud_dates)
-
-
+                # Print every 10 objects to a new file
+                if i % 10 == 0:
+                    new_filename = base_file_name + "_" + str(int(time.time())) + ".csv"
+                    with open(new_filename, "a+") as text_file:
+                        print("{}".format(csv_lines), file=text_file)
+                    csv_lines = (''.join([h + '|' for h in headers])[:-1] + '\n')
 
